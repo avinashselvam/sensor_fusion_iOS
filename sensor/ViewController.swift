e492cd85a4961685ee2ce9645e6ad545d134ab9b
avinashselvam <avinashpanneer@gmail.com>
2018-10-02 16:55:31 +0530

    big changes refactored lot of code now DRY af

--- sensor/ViewController.swift
+++ sensor/ViewController.swift
@@ -12,257 +12,176 @@ import SceneKit
 
 class ViewController: UIViewController {
     
-    var alreadyRunning = true
-    
-    var a = 0.0, b = 0.0, c = 0.0
-    
-    var blue : UIColor!
-    
-    var ame = SCNVector3(0.0, 0.0, 0.0)
-    var ge = SCNVector3(0.0, 0.0, 0.0)
-    
-    var calibOffset = SCNVector3(0.0, 0.0, 0.0)
-    
-    @IBOutlet weak var startBtn: UILabel!
-    
-    @objc func onTap(){
-        if alreadyRunning {
-            stopUpdates()
-            startBtn.text = "Start"
-            startBtn.textColor = self.blue
-            self.alreadyRunning = false
-        }
-        else{
-            startUpdates()
-            startBtn.text = "Stop"
-            startBtn.textColor = UIColor.red
-            self.alreadyRunning = true
-        }
-    }
-    @IBAction func calibrate(_ sender: UIButton) {
-        initGyro()
-        if let data = self.motionManager.gyroData {
-            
-            let x = data.rotationRate.x
-            let y = data.rotationRate.y
-            let z = data.rotationRate.z
-            
-            self.calibOffset = SCNVector3(x, y, z)
-            
-        }
-    }
-    
-//    filtered estimate
+    //    filtered estimate
     @IBOutlet weak var Fz: UILabel!
     @IBOutlet weak var Fy: UILabel!
     @IBOutlet weak var Fx: UILabel!
     
-//    gyro estimate
+    //    gyro estimate
     @IBOutlet weak var GEz: UILabel!
     @IBOutlet weak var GEy: UILabel!
     @IBOutlet weak var GEx: UILabel!
     
-    
-//    Accelerometer + Magnetometer estimate
+    //    Accelerometer + Magnetometer estimate
     @IBOutlet weak var AMEz: UILabel!
     @IBOutlet weak var AMEy: UILabel!
     @IBOutlet weak var AMEx: UILabel!
     
-//    gyro
+    //    gyro
     @IBOutlet weak var Gz: UILabel!
     @IBOutlet weak var Gy: UILabel!
     @IBOutlet weak var Gx: UILabel!
     
-//    accelerometer
+    //    accelerometer
     @IBOutlet weak var Az: UILabel!
     @IBOutlet weak var Ay: UILabel!
     @IBOutlet weak var Ax: UILabel!
     
-//    magnetometer
+    //    magnetometer
     @IBOutlet weak var Mz: UILabel!
     @IBOutlet weak var My: UILabel!
     @IBOutlet weak var Mx: UILabel!
     
+//  <----- CODE STARTS FROM HERE ----->
+    
     let motionManager = CMMotionManager()
     var timer = Timer()
     
-    func compFilter(factor f: Float) {
-        
-        let ge = self.ge, ame = self.ame
-        
-        let c = (180/Float.pi)
-        
-        let x = f*ge.x*c + (1-f)*ame.x
-        let y = f*ge.y*c + (1-f)*ame.y
-        let z = f*ge.z*c + (1-f)*ame.z
-        
-        self.Fx.text = String(format: "%.3f", x)
-        self.Fy.text = String(format: "%.3f", y)
-        self.Fz.text = String(format: "%.3f", z)
-        
+    var g = float3(0,0,0) { didSet{ setLabels([Gx, Gy, Gz], with: g) } }
+    var a = float3(0,0,0) { didSet{ setLabels([Ax, Ay, Az], with: a) } }
+    var m = float3(0,0,0) { didSet{ setLabels([Mx, My, Mz], with: m) } }
+    
+    var ge = float3(0,0,0) { didSet{ setLabels([GEx, GEy, GEz], with: ge) } }
+    var ame = float3(0,0,0) { didSet{ setLabels([AMEx, AMEy, AMEz], with: ame) } }
+    
+//    app's state i.e if sensor data is being received
+    var isRunning = true
+    
+//    the two buttons in the app
+    @IBAction func calibrate(_ sender: UIButton) {
+        initGyro()
     }
     
-    func kalmanFilter(){
-        
-    }
-    
-    func stopUpdates() {
-        self.timer.invalidate()
-        self.motionManager.stopGyroUpdates()
-        self.motionManager.stopAccelerometerUpdates()
-        self.motionManager.stopMagnetometerUpdates()
-    }
-    
-    func startUpdates() {
-        if motionManager.isAccelerometerAvailable{
-            self.motionManager.accelerometerUpdateInterval = 1.0/60
-            self.motionManager.startAccelerometerUpdates()
+    @IBAction func start(_ sender: UIButton) {
+        isRunning = !isRunning
+        if isRunning {
+            stopUpdates()
+            sender.setTitle("Start", for: .normal)
+            sender.setTitleColor(UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1), for: .normal)
         }
-        
-        if motionManager.isMagnetometerAvailable{
-            self.motionManager.magnetometerUpdateInterval = 1.0/60
-            self.motionManager.startMagnetometerUpdates()
+        else {
+            startUpdates()
+            sender.setTitle("Stop", for: .normal)
+            sender.setTitleColor(UIColor.red, for: .normal)
         }
-        
-        if motionManager.isGyroAvailable{
-            self.motionManager.gyroUpdateInterval = 1.0/60
-            self.motionManager.startGyroUpdates()
-        }
-        
-        self.timer = Timer(fire: Date(), interval: 1.0/60.0, repeats: true, block:{ (timer) in
-            
-            if let data = self.motionManager.gyroData {
-                
-                let x = (data.rotationRate.x - Double(self.calibOffset.x)).roundedTo(decimalPlaces: 2)
-                let y = (data.rotationRate.y - Double(self.calibOffset.y)).roundedTo(decimalPlaces: 2)
-                let z = (data.rotationRate.z - Double(self.calibOffset.z)).roundedTo(decimalPlaces: 2)
-                
-                self.Gx.text = String(format: "%.3f", x)
-                self.Gy.text = String(format: "%.3f", y)
-                self.Gz.text = String(format: "%.3f", z)
-                
-//                update alpha, beta, gamma
-                self.a += x*(timer.timeInterval)
-                self.b += y*(timer.timeInterval)
-                self.c += z*(timer.timeInterval)
-                
-                let c = (180/Double.pi)
-                
-                self.GEx.text = String(format: "%.3f", self.a*c)
-                self.GEy.text = String(format: "%.3f", self.b*c)
-                self.GEz.text = String(format: "%.3f", self.c*c)
-                
-                self.ge = SCNVector3(self.a, self.b, self.c)
-                
-                
-            }
-            
-            if let data = self.motionManager.magnetometerData {
-                
-                let x = Float(data.magneticField.x)
-                let y = Float(data.magneticField.y)
-                let z = Float(data.magneticField.z)
-                
-                self.Mx.text = String(format: "%.2f", x)
-                self.My.text = String(format: "%.2f", y)
-                self.Mz.text = String(format: "%.2f", z)
-                
-                let c = Float.pi/180
-                let roll = self.ame.x*c, pitch = self.ame.z*c
-                
-                let Yh = (y * cos(roll)) - (z * sin(roll));
-                let Xh = (x * cos(pitch))+(y * sin(roll)*sin(pitch)) + (z * cos(roll) * sin(pitch));
-                
-                let yaw = atan(Yh/Xh)*(180/Float.pi)
-                
-                
-                self.AMEy.text = String(format: "%.3f", yaw)
-                
-                self.ame.y = Float(yaw)
-            }
-            
-            if let data = self.motionManager.accelerometerData {
-                
-                let x = data.acceleration.x
-                let y = data.acceleration.y
-                let z = data.acceleration.z
-                
-                self.Ax.text = String(format: "%.3f", x)
-                self.Ay.text = String(format: "%.3f", y)
-                self.Az.text = String(format: "%.3f", z)
-                
-                let pitch = atan(-y/pow(pow(x, 2) + pow(z,2), 0.5))*(180/Double.pi)
-                let roll = atan(x/z)*(180/Double.pi)
-                
-                self.AMEx.text = String(format: "%.3f", pitch)
-                self.AMEz.text = String(format: "%.3f", roll)
-                
-                self.ame.x = Float(pitch)
-                self.ame.z = Float(roll)
-                
-            }
-            
-            self.compFilter(factor: 0.9)
-            
-        })
-        
-        RunLoop.current.add(self.timer, forMode: RunLoop.Mode.default)
     }
     
     func initGyro(){
-        if let data = self.motionManager.accelerometerData {
-            
-            let x = data.acceleration.x
-            let y = data.acceleration.y
-            let z = data.acceleration.z
-            
-            let pitch = atan(-y/pow(pow(x, 2) + pow(z,2), 0.5))
-            let roll = atan(x/z)
-            
-            self.a = pitch
-            self.c = roll
-            
-        }
-        
-        if let data = self.motionManager.magnetometerData {
-            
-            let x = Float(data.magneticField.x)
-            let y = Float(data.magneticField.y)
-            let z = Float(data.magneticField.z)
-            
-            let c = Float.pi/180
-            let roll = self.ame.x*c, pitch = self.ame.z*c
-            
-            let Yh = (y * cos(roll)) - (z * sin(roll))
-            let Xh = (x * cos(pitch))+(y * sin(roll)*sin(pitch)) + (z * cos(roll) * sin(pitch))
-            
-            let yaw = atan(Yh/Xh)
-            
-            self.b = Double(yaw)
+        //  gyro needs initial values since it measures only angular velocity.
+        //  So we init its estimate with acc + mag estimate
+        g = float3(0,0,0)
+        ge = ame
+    }
+    
+    func setLabels(_ labels: [UILabel], with values: float3){
+        for i in 0..<3 {
+            labels[i].text = "\(values[i])"
         }
     }
-
+    
+    func getsetSensorValues(){
+        guard let accValues = motionManager.getAcc() else { return }
+        a = accValues
+        guard let magValues = motionManager.getMag() else { return }
+        m = magValues
+        guard let gyroValues = motionManager.getGyro() else { return }
+        g = gyroValues
+    }
+    
+    func calcEstimates(){
+        //       x -> pitch, y -> roll, z -> yaw
+        let dt = timer.timeInterval.f(3)
+        ge += float3(g.x*dt, g.y*dt, g.z*dt)
+        
+        
+        
+    }
+    
+    func startUpdates() {
+        motionManager.startSensors(withFrequency: 1.0/60)
+        timer = Timer(fire: Date(), interval: 1.0/60.0, repeats: true, block:{ (timer) in
+            self.getsetSensorValues()
+            self.calcEstimates()
+        })
+        //        adds timer to current thread so that it's executed at constant frequency
+        RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+    }
+    
+    func stopUpdates() {
+        timer.invalidate()
+        motionManager.stopSensors()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.blue = startBtn.textColor
         initGyro()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(onTap))
-        tap.numberOfTapsRequired = 2
-        self.view.addGestureRecognizer(tap)
     }
 
 
 }
 
 extension Double {
-    func roundedTo(decimalPlaces: Int) -> Double {
-        let formattedString = String(format: "%.\(decimalPlaces)f", self) as String
-        return Double(formattedString)!
+    func f(_ decimalPlaces: Int) -> Float {
+        let formattedString = String(format: "%.\(decimalPlaces)f", self)
+        return Float(formattedString)!
     }
 }
 
+extension CMMotionManager {
+    
+    func getGyro() -> float3?{
+        guard let data = self.gyroData else { return nil }
+        let x = data.rotationRate.x, y = data.rotationRate.y, z = data.rotationRate.z
+        return float3(x: x.f(2), y: y.f(2), z: z.f(2))
+    }
+    
+    func getAcc() -> float3?{
+        guard let data = self.accelerometerData else { return nil }
+        let x = data.acceleration.x, y = data.acceleration.y, z = data.acceleration.z
+        return float3(x: x.f(2), y: y.f(2), z: z.f(2))
+    }
+    
+    func getMag() -> float3?{
+        guard let data = self.magnetometerData else { return nil }
+        let x = data.magneticField.x, y = data.magneticField.y, z = data.magneticField.z
+        return float3(x: x.f(2), y: y.f(2), z: z.f(2))
+    }
+    
+    func stopSensors(){
+        self.stopGyroUpdates()
+        self.stopAccelerometerUpdates()
+        self.stopMagnetometerUpdates()
+    }
+    
+    func startSensors(withFrequency seconds: Double){
+        if self.isAccelerometerAvailable{
+            self.accelerometerUpdateInterval = seconds
+            self.startAccelerometerUpdates()
+        }
+        
+        if self.isMagnetometerAvailable{
+            self.magnetometerUpdateInterval = seconds
+            self.startMagnetometerUpdates()
+        }
+        
+        if self.isGyroAvailable{
+            self.gyroUpdateInterval = seconds
+            self.startGyroUpdates()
+        }
+    }
+
+}
